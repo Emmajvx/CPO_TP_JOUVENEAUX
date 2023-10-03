eda840d293f469880ae719b13d82acadf4c32d02
emma jouveneaux <emma.jouveneaux@epfedu.fr>
2023-10-03 21:33:32 +0200

    je viens de finir l'exercice de "guess my number" avec le bonus qui semble fonctionnel

--- TP1_guessMyNumber_JOUVENEAUX/src/tp1_guessmynumber_jouveneaux/TP1_guessMyNumber_JOUVENEAUX.java
+++ TP1_guessMyNumber_JOUVENEAUX/src/tp1_guessmynumber_jouveneaux/TP1_guessMyNumber_JOUVENEAUX.java
@@ -19,27 +19,96 @@ public class TP1_guessMyNumber_JOUVENEAUX {
     public static void main(String[] args) {
         int test=0;
         int number=1000;
-        int essai=0;
+        int essai=1;
+        int choix;
+        int alea;
         
         Scanner sc = new Scanner(System.in);
         Random generateurAleat = new Random();
         
         number = generateurAleat.nextInt(100);
+        System.out.println("\n Veuillez seectionner un niveau de difficulte: \n 1) Facile, nombre illimite de tentatives \n 2) Moyen, 20 tentatives \n 3) Difficile, seulement 7 tentatives \n 4) Cauchemar");
+        choix = sc.nextInt();
         System.out.println("\n Veuillez saisir un nombre entre 1 et 100:");
         test = sc.nextInt();
-        while (number!=test){
-            essai=essai+1;
-            if (number>test){
-                System.out.println("\n Votre nombre est trop petit, veuillez essayer a nouveau:");
-                test = sc.nextInt();
+        
+        if (choix==1){
+            while (number!=test){
+                essai=essai+1;
+                if (number>test){
+                    System.out.println("\n Votre nombre est trop petit, veuillez essayer a nouveau:");
+                    test = sc.nextInt();
+                }
+                if (number<test){
+                    System.out.println("\n Votre nombre est trop grand, veuillez essayer a nouveau:");
+                    test = sc.nextInt();
+                }
             }
-            if (number<test){
-                System.out.println("\n Votre nombre est trop grand, veuillez essayer a nouveau:");
-                test = sc.nextInt();
-            }
+            System.out.println("\n Vous avez reussi en "+(essai+1)+"! \n le nombre etait "+number);
         }
-        System.out.println("\n Vous avez reussi en "+essai+"! \n le nombre etait "+number);
-
+        
+        if (choix==2){
+            while (number!=test){
+                essai=essai+1;
+                if (number>test && essai<21){
+                    System.out.println("\n Votre nombre est trop petit, veuillez essayer a nouveau:");
+                    test = sc.nextInt();
+                }
+                if (number<test){
+                    System.out.println("\n Votre nombre est trop grand, veuillez essayer a nouveau:");
+                    test = sc.nextInt();
+                }
+                if (essai>20){
+                    System.out.println("\n Perdu! Vous avez atteint le nombre maximum de tentatives");
+                }
+            }
+            System.out.println("\n Vous avez reussi en "+(essai+1)+"! \n le nombre etait "+number);
+        }
+        if (choix==3){
+            while (number!=test){
+                essai=essai+1;
+                if (number>test && essai<8){
+                    System.out.println("\n Votre nombre est trop petit, veuillez essayer a nouveau:");
+                    test = sc.nextInt();
+                }
+                if (number<test){
+                    System.out.println("\n Votre nombre est trop grand, veuillez essayer a nouveau:");
+                    test = sc.nextInt();
+                }
+                if (essai>7){
+                    System.out.println("\n Perdu! Vous avez atteint le nombre maximum de tentatives");
+                }
+            }
+            System.out.println("\n Vous avez reussi en "+(essai+1)+"! \n le nombre etait "+number);
+        }
+        if (choix==4){
+            while (number!=test){
+                essai=essai+1;
+                if (number>test){
+                    alea = generateurAleat.nextInt(3);
+                    if (alea==1 || alea==2){
+                     System.out.println("\n Votre nombre est trop grand, veuillez essayer a nouveau:");
+                    test = sc.nextInt();    
+                    }
+                    if (alea==3){
+                    System.out.println("\n Votre nombre est trop petit, veuillez essayer a nouveau:");
+                    test = sc.nextInt();
+                    }
+                }
+                if (number<test){
+                    alea = generateurAleat.nextInt(3);
+                    if (alea==3){
+                     System.out.println("\n Votre nombre est trop grand, veuillez essayer a nouveau:");
+                    test = sc.nextInt();    
+                    }
+                    if (alea==1 || alea==2){
+                    System.out.println("\n Votre nombre est trop petit, veuillez essayer a nouveau:");
+                    test = sc.nextInt();
+                    }
+                }
+            }
+            System.out.println("\n Vous avez reussi en "+(essai+1)+"! \n le nombre etait "+number);
+        }
     }
     
 }
